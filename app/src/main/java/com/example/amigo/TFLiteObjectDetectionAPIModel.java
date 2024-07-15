b6217e81ed632d0b84e6b17ceb89e8736b8af1ce
Nimita Gurjar <gurjarnimita@gmail.com>
2024-07-15 23:30:42 +0530

    version 0.1

--- app/src/main/java/com/example/amigo/TFLiteObjectDetectionAPIModel.java
+++ app/src/main/java/com/example/amigo/TFLiteObjectDetectionAPIModel.java
@@ -21,6 +21,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 public class TFLiteObjectDetectionAPIModel {
 
     private static final String TAG = "TFLiteObjectDetection";
